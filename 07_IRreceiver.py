da50c9844708367d4c4b28a96cc285cc93791014
Delwin Padilla <delwinjpp@gmail.com>
2024-10-21 12:00:05 +0200

    update code 7

--- 07_IRreceiver.py
+++ 07_IRreceiver.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python
-from gpiozero import DigitalInputDevice
-import time
-from signal import pause
+# Specify the Python interpreter.
 
-IrPin=6
-count=0
-IRrec=DigitalInputDevice(IrPin, pull_up=True)
+from gpiozero import DigitalInputDevice
+# Import DigitalInputDevice for IR receiver.
+
+from signal import pause
+# Import pause to wait indefinitely.
+
+IrPin = 6
+# Set GPIO pin number for IR receiver.
+
+count = 0
+# Initialize signal count.
+
+IRrec = DigitalInputDevice(IrPin, pull_up=True)
+# Create an IR receiver instance with pull-up resistor.
 
 def cont():
-   
+    # Function to handle received signals.
+    
     global count
-    count +=1
+    # Use global count variable.
+
+    count += 1
+    # Increment count.
+
     print("Received infrared. cnt=", count)
+    # Print the count of received signals.
 
-
-if __name__ == '__main__': # Program start from here
+if __name__ == '__main__':  # Start of the program.
     try:
-        IRrec.when_deactivated=cont
+        # Try block for exception handling.
+
+        IRrec.when_deactivated = cont
+        # Set event to call cont on signal detection.
+
         pause()
-    except KeyboardInterrupt: # When 'Ctrl+C' program ended
-        print("\n Program ended")   
+        # Wait indefinitely for signals.
+
+    except KeyboardInterrupt:  # Handle Ctrl+C interrupt.
+        print("\n Program ended")
+        # Print end message.
+
         IRrec.close()
-
-
+        # Close the IR receiver.
